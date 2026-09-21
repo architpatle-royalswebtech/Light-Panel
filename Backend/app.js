bda2d119ab5d7f47bb9558cc9480fc4994693bf3
Archit Patle <architpatle06@gmail.com>
2026-09-21 16:26:31 +0530

    updated cors for new vercel link

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const routers = require("./router")
 const allowedOrigins = [
     "http://localhost:5173",
     "https://lightpanelupdated.netlify.app",
-    "https://light-panel-six.vercel.app/"
+    "https://light-panel-six.vercel.app"
 
 ];
 
